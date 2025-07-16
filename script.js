5a843460ba26e1a60642825425c54bf700f44fbc
Rohit Kumar <rkcpratap@gmail.com>
2025-07-16 12:55:38 +0530

    reCaptcha Validation

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function beforeSubmit() {
-  const outputDate = document.querySelector(".outputdate");
-  const inputDate = document.querySelector(".inputDate");
-  console.log(inputDate.value);
-  let formateddate = new Date(inputDate.value).toLocaleDateString("en-IN");
-  outputDate.value = formateddate;
+let captchaChecked = false;
+function beforeSubmit(event) {
+  if (!captchaChecked) {
+    const outputDate = document.querySelector(".outputdate");
+    const inputDate = document.querySelector(".inputDate");
+    console.log(inputDate.value);
+    let formateddate = new Date(inputDate.value).toLocaleDateString("en-IN");
+    outputDate.value = formateddate;
+  } else {
+    alert("Please Check the reCAPTCHA box before submitting.");
+    event.preventDefault();
+  }
 }
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
@@ -17,3 +23,6 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+function captchasuccess() {
+  captchaChecked = true;
+}
